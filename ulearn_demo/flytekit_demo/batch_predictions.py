00af5cb7852c7557494ffb2396bccd5ca4654af9
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-04-28 14:44:52 -0700

    Move input generate to tasks

--- ulearn_demo/flytekit_demo/batch_predictions.py
+++ ulearn_demo/flytekit_demo/batch_predictions.py
@@ -1,9 +1,11 @@
+import random
 import torch
 import torch.nn
-from typing import List, NamedTuple
+from typing import List
 from flytekit import workflow, map_task, reference_task, task
 
 from pictionary_app.dataset import QuickDrawDataset
+from pictionary_app.dataset import get_quickdraw_class_names
 from pictionary_app.main import model, predictor
 
 
@@ -25,3 +27,25 @@ def mappable_task(input: MapItem) -> dict:
 
 
 batch_predictions_task = map_task(mappable_task)
+
+
+@task(cache=True, cache_version="v1")
+def download_quickdraw_class_names() -> List[str]:
+   return get_quickdraw_class_names()
+
+
+@task(cache=True, cache_version="v1")
+def download_quickdraw_dataset(max_items_per_class: int, num_classes: int) -> QuickDrawDataset:
+   return QuickDrawDataset("/tmp/quickdraw_data", max_items_per_class=max_items_per_class, class_limit=num_classes)
+
+
+@task
+def generate_input(n_entries: int, dataset: QuickDrawDataset, class_names: List[str], max_items_per_class: int, num_classes: int) -> (List[torch.Tensor], List[str]):
+   feature_list = []
+   label_list = []
+   # Grab a few random entries from the original dataset
+   for i in range(n_entries):
+      X, y = dataset[random.randint(0, max_items_per_class * num_classes)]
+      feature_list.append(X.squeeze())
+      label_list.append(class_names[y])
+   return feature_list, label_list

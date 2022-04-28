2ec652b8961f976c2ec8d4fc20f0afa1e33b5a1f
Haytham Abuelfutuh <haytham@afutuh.com>
2022-04-28 13:13:38 -0600

    Separate map task
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- ulearn_demo/flytekit_demo/batch_predictions.py
+++ ulearn_demo/flytekit_demo/batch_predictions.py
@@ -24,15 +24,4 @@ def mappable_task(input: MapItem) -> dict:
     return predictor(input.model_object, features)
 
 
-@task
-def run_batch_predictions(map_input: List[MapItem]) -> List[dict]:
-    return map_task(mappable_task)(input=map_input)
-
-
-@workflow
-def wf(
-    model_object: torch.nn.Module,
-    feature_list: List[torch.Tensor],
-) -> List[dict]:
-    map_input = prepare_map_inputs(model_object=model_object, feature_list=feature_list)
-    return run_batch_predictions(map_input=map_input)
+batch_predictions_task = map_task(mappable_task)

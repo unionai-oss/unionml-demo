4b85e3e49860ceafd3a5cf351610febb88a35d0c
Niels Bantilan <niels.bantilan@gmail.com>
2022-05-03 14:52:30 -0400

    re-run training
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- unionml_demo/tmp.py
+++ unionml_demo/tmp.py
@@ -5,7 +5,7 @@ num_classes = 345
 # run training on a remote
 execution = model.remote_train(
     hyperparameters={"num_classes": num_classes},
-    trainer_kwargs={"num_epochs": 1, "batch_size": 4096},
+    trainer_kwargs={"num_epochs": 20, "batch_size": 8192},
     data_dir="./data",
     max_examples_per_class=10000,
     class_limit=num_classes,

4509518c8d6c64c2d14701a89ed29d3370feb3b6
Niels Bantilan <niels.bantilan@gmail.com>
2022-06-15 17:13:02 -0400

    update demo notebooks
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- unionml_demo/train_remote.py
+++ unionml_demo/train_remote.py
@@ -4,6 +4,7 @@ num_classes = 345
 
 # run training on a remote
 execution = model.remote_train(
+    wait=False,
     hyperparameters={"num_classes": num_classes},
     trainer_kwargs={"num_epochs": 20, "batch_size": 256},
     data_dir="./data",
